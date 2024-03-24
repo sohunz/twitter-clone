73761ef4c29cbbe0c8e9729d84673cde320c6488
sohunz <sohunily@gmail.com>
2024-03-24 21:00:12 +0700

    communities done

--- src/pages/sidebar/community/Community.jsx
+++ src/pages/sidebar/community/Community.jsx
@@ -7,6 +7,34 @@ const Explore = () => {
             <div className="sticky z-50 top-0">
                 <CommunityNav />
             </div>
+            <div>
+                <p className="px-5 mt-5 font-bold text-xl mb-10">
+                    Discover Communities
+                </p>
+                <div className="px-5 flex gap-5 hover:bg-[#1E2732] py-4">
+                    <div className=" rounded-xl overflow-hidden w-[100px] h-[100px]">
+                        <img
+                            src="https://qph.cf2.quoracdn.net/main-qimg-68d52a4c9747f85b66e6b385c1c707d2-lq"
+                            alt=""
+                            className="w-full h-full object-cover"
+                        />
+                    </div>
+                    <div className="flex flex-col justify-between">
+                        <div>
+                            <p className="font-bold">Olivia Rodrigo</p>
+                            <p className="text-sm">
+                                40K{" "}
+                                <span className="text-gray-400">Members</span>
+                            </p>
+                        </div>
+                        <div className="inline-flex">
+                            <p className=" bg-[#1D9BF0] px-5 py-1 text-sm rounded-full cursor-pointer">
+                                Join
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
