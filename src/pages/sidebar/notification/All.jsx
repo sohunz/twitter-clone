81ba6238be471325b19e14f41032b52277391824
sohunz <sohunily@gmail.com>
2024-03-28 15:03:41 +0700

    change some

--- src/pages/sidebar/notification/All.jsx
+++ src/pages/sidebar/notification/All.jsx
@@ -13,17 +13,17 @@ const All = () => {
                             <div className="flex gap-3">
                                 <div className="rounded-full overflow-hidden w-[45px] h-[45px]">
                                     <img
-                                        src="https://qph.cf2.quoracdn.net/main-qimg-68d52a4c9747f85b66e6b385c1c707d2-lq"
+                                        src="https://admin.itsnicethat.com/images/_fsej_vmVYPAz138de7knz6lEFA=/243516/format-webp%7Cwidth-1440/twitter-x-logo-graphic-design-itsnicethat-01.jpeg"
                                         alt=""
                                         className="w-full h-full object-cover"
                                     />
                                 </div>
                                 <div>
                                     <p className="font-bold text-[16px]">
-                                        Olivia Rodrigo
+                                        Sohunz
                                     </p>
                                     <p className="text-gray-400 text-sm">
-                                        @sohun
+                                        @sohunz
                                     </p>
                                 </div>
                             </div>
